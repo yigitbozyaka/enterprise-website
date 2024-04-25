4c0d38e58b3b9d911c4908d1183a409f6f404ec0
Yiğit Bozyaka <112256994+yigitbozyaka@users.noreply.github.com>
2024-04-26 00:43:40 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,16 +2,22 @@ const pricing = document.getElementById("pricing");
 const features = document.getElementById("features");
 const pricingBtns = document.querySelectorAll(".pricing-btn");
 const featuresBtns = document.querySelectorAll(".features-btn");
+const mobileMenuIcon = document.getElementById("mobile-menu-icon");
+const mobileMenu = document.getElementById("mobile-menu");
+
+mobileMenu.style.display = "none";
 
 pricingBtns.forEach(btn => {
   btn.addEventListener("click", () => {
     smoothScrollTo(pricing, "pricing");
+    mobileMenu.style.display = "none";
   });
 });
 
 featuresBtns.forEach(btn => {
   btn.addEventListener("click", () => {
     smoothScrollTo(features, "features");
+    mobileMenu.style.display = "none";
   });
 });
 
@@ -26,3 +32,7 @@ function smoothScrollTo(element, path) {
     window.location.href = "/index.html#" + path;
   }
 }
+
+mobileMenuIcon.addEventListener("click", () => {
+  mobileMenu.style.display = mobileMenu.style.display === "flex" ? "none" : "flex";
+});
